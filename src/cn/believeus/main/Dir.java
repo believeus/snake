62c3bf1d420406fbf3649774036c9233aa625bf4
wuqiwei <1058633117@qq.com>
2016-05-14 07:35:00 +0800

    init

---
+++ src/cn/believeus/main/Dir.java
@@ -0,0 +1,5 @@
+package cn.believeus.main;
+
+public enum Dir {
+	right,left,up,down
+}
---
+++ src/cn/believeus/main/Main.java
@@ -0,0 +1,9 @@
+package cn.believeus.main;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Yard yard=new Yard();
+		yard.lauch();
+	}
+}
